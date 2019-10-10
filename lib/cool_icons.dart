9cbeb6cf802f88f4c75ba58cf9488909db8e1529
Anton Antonov <contact@fin.design>
2019-10-10 19:46:08 +0300

    initial commit

---
+++ lib/cool_icons.dart
@@ -0,0 +1,17 @@
+/// flutter:
+///   fonts:
+///    - family:  CoolIcons
+///      fonts:
+///       - asset: fonts/CoolIcons.ttf
+///
+/// 
+///
+import 'package:flutter/widgets.dart';
+
+class CoolIcons {
+	CoolIcons._();
+
+	static const _kFontFam = 'CoolIcons';
+
+	static const IconData foreground = const IconData( 0xe000, fontFamily: _kFontFam );
+}
